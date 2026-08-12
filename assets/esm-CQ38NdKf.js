71c42a445899b506144c732a49ffed573aef75d5
Garrett Duncan <duncansports_17@hotmail.com>
2026-08-12 08:19:10 -0500

    deploy: afflictions + heal rules (gametest 6c269cee)

---
+++ assets/esm-CQ38NdKf.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-DNv-EJSZ.js","./dist-6REUuaFv.js"])))=>i.map(i=>d[i]);
+import{t as e}from"./preload-helper-Chd9yIcd.js";import{n as t}from"./dist-6REUuaFv.js";var n=t(`Preferences`,{web:()=>e(()=>import(`./web-DNv-EJSZ.js`).then(e=>new e.PreferencesWeb),__vite__mapDeps([0,1]),import.meta.url)});export{n as Preferences};
